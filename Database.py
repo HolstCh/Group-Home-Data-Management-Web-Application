06a6db7d26961b62da01cef472b2c1cf6e711ad8
Syntax-Cipher <chad.holst@hotmail.com>
2021-12-13 11:31:29 -0700

    Login Validation

--- Database.py
+++ Database.py
@@ -14,10 +14,23 @@ class Database:
     except Error as e:
         print(e)
 
-    def selectAll(self):
+    def selectAllAccounts(self):
         connection = mysql.connect()
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM ACCOUNT")
         records = cursor.fetchall()
         for i in records:
             print(i)
+
+    def verifyAccount(self, username, password):
+        connection = mysql.connect()
+        cursor = connection.cursor()
+        query = "SELECT * FROM ACCOUNT WHERE username = %s AND password = %s"
+        info = (username, password)
+        cursor.execute(query, info)
+        record = cursor.fetchone()
+        if not record:
+            print('Error: No account')
+        else:
+            return True
+

9f49d8206927b663661113a24d217906902890e3
Syntax-Cipher <chad.holst@hotmail.com>
2021-12-13 19:51:31 -0700

    Account Login Verification with no JSON

--- Main.py
+++ Main.py
@@ -3,16 +3,19 @@ from DatabaseConfig import mysql
 from App import app
 
 
+# redirect directs to different page based on url_for
+
 @app.route("/")
 def main():
     return redirect(url_for("home"))
 
+
 @app.route("/home", methods=["POST", "GET"])
 def home():
-    if  request.method == 'POST':
+    if request.method == 'POST':
         user = request.form["inputUsername"]
         password = request.form["inputPassword"]
-        return redirect(url_for("account", usr=user))
+        return redirect(url_for("verifyAccount", username=user, password=password))
     else:
         return render_template('designApp.html')
 
@@ -31,24 +34,20 @@ def account(usr):
     return render_template('designAcc.html', usr=usr)
 
 
-@app.route("/verifyAccount/<username>/<password>")
+@app.route("/verifyAccount/<username>/<password>", methods=['POST'])
 def verifyAccount(username, password):
-    try:
-        connection = mysql.connect()
-        cursor = connection.cursor()
-        query = "SELECT * FROM ACCOUNT WHERE username = %s AND password = %s", username, password
-        cursor.execute(query)
-        canLogin = cursor.fetchone()
-        checkRow = canLogin.rowCount
-        if checkRow is None:
-            return redirect(url_for("home"))
-        else:
-            return redirect(url_for("account", usr=username))
-    except Exception as e:
-        print(e)
-    finally:
-        cursor.close()
-        connection.close()
+    connection = mysql.connect()
+    cursor = connection.cursor()
+    query = "SELECT * FROM ACCOUNT WHERE username = %s AND password = %s"
+    info = (username, password)
+    cursor.execute(query, info)
+    canLogin = cursor.fetchone()
+    if not canLogin:
+        print('Error: no account')
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("account", usr=username))
+
 
 #  if request.method == 'GET':
 #       hasAccount = redirect(url_for("verifyAccount", username=user, password=password))

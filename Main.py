aa3b726b4cd6dd000f65481aadf22ec8951e5517
Syntax-Cipher <chad.holst@hotmail.com>
2021-12-19 14:51:17 -0700

    Sign out button, loaded share codes, view Document queries

--- Main.py
+++ Main.py
@@ -26,8 +26,8 @@ def home():
         return render_template('designApp.html')
 
 
-@app.route("/create-account", methods=["POST", "GET"])
-def newaccount():
+@app.route("/newAccount", methods=["POST", "GET"])
+def newAccount():
     if request.method == 'POST':
         user = request.form["inputUsername"]
         return redirect(url_for("account", usr=user))  # go to user's account page
@@ -52,16 +52,17 @@ def accountPsy(usr, profession):
 
 @app.route("/loadPhysicalCodes/<SIN>/<username>/<profession>")  # load user physical codes into local array
 def loadPhysicalCodes(SIN, username, profession):
-    x = requests.get("http://127.0.0.1:5000/physicalCodes/" + SIN)
-    allPhysicalCodes = json.loads(x.text)
+    codes = requests.get("http://127.0.0.1:5000/physicalCodes/" + SIN)
+    allPhysicalCodes = json.loads(codes.text)
     print(allPhysicalCodes)
+    print(type(allPhysicalCodes))
     return redirect(url_for("loadLogCodes", SIN=SIN, username=username, profession=profession))
 
 
 @app.route("/loadLogCodes/<SIN>/<username>/<profession>")  # load user log codes into local array
 def loadLogCodes(SIN, username, profession):
-    x = requests.get("http://127.0.0.1:5000/logCodes/" + SIN)
-    allLogCodes = json.loads(x.text)
+    codes = requests.get("http://127.0.0.1:5000/logCodes/" + SIN)
+    allLogCodes = json.loads(codes.text)
     print(allLogCodes)
     return redirect(url_for("loadMentalCodes", SIN=SIN, username=username, profession=profession))
 
@@ -69,8 +70,8 @@ def loadLogCodes(SIN, username, profession):
 @app.route(
     "/loadMentalCodes/<SIN>/<username>/<profession>")  # load user mental codes into local array and then directs to professional specific page
 def loadMentalCodes(SIN, username, profession):
-    x = requests.get("http://127.0.0.1:5000/mentalCodes/" + SIN)
-    allMentalCodes = json.loads(x.text)
+    codes = requests.get("http://127.0.0.1:5000/mentalCodes/" + SIN)
+    allMentalCodes = json.loads(codes.text)
     print(allMentalCodes)
     if profession == 'Youth Worker':
         return redirect(url_for("accountYouth", usr=username, profession=profession))
@@ -83,33 +84,45 @@ def loadMentalCodes(SIN, username, profession):
 @app.route("/verifyAccount/<username>/<password>/<profession>",
            methods=['GET'])  # verifies user and then loads their share codes into a local array
 def verifyAccount(username, password, profession):
-    connection = mysql.connect()
-    cursor = connection.cursor()
-    query = "SELECT SIN FROM ACCOUNT as A, HAS as H WHERE A.username = %s AND A.password = %s" \
-            " AND A.professionName = %s AND A.username = H.username"
-    info = (username, password, profession)
-    cursor.execute(query, info)
-    sin = cursor.fetchone()
-    if not sin:
-        print('Error: no account')
-        return redirect(url_for("home"))
-    else:
-        return redirect(url_for("loadPhysicalCodes", SIN=sin, username=username, profession=profession))
+    try:
+        connection = mysql.connect()
+        cursor = connection.cursor()
+        query = "SELECT SIN FROM ACCOUNT as A, HAS as H WHERE A.username = %s AND A.password = %s" \
+                " AND A.professionName = %s AND A.username = H.username"
+        info = (username, password, profession)
+        cursor.execute(query, info)
+        sin = cursor.fetchone()
+        if not sin:
+            print('Error: no account')
+            return redirect(url_for("home"))
+        else:
+            return redirect(url_for("loadPhysicalCodes", SIN=sin, username=username, profession=profession))
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        connection.close()
 
 
 @app.route("/verifyProfession/<username>/<password>",
            methods=['GET'])  # calls verifyAccount() to direct to the professional's page
 def verifyProfession(username, password):
-    connection = mysql.connect()
-    cursor = connection.cursor()
-    query = "SELECT professionName FROM ACCOUNT WHERE username = %s AND password = %s"
-    info = (username, password)
-    cursor.execute(query, info)
-    profType = cursor.fetchone()
-    if not profType:
-        return redirect(url_for("home"))
-    else:
-        return redirect(url_for("verifyAccount", username=username, password=password, profession=profType))
+    try:
+        connection = mysql.connect()
+        cursor = connection.cursor()
+        query = "SELECT professionName FROM ACCOUNT WHERE username = %s AND password = %s"
+        info = (username, password)
+        cursor.execute(query, info)
+        profType = cursor.fetchone()
+        if not profType:
+            return redirect(url_for("home"))
+        else:
+            return redirect(url_for("verifyAccount", username=username, password=password, profession=profType))
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        connection.close()
 
 
 @app.route("/logCodes/<SIN>", methods=['GET'])
@@ -172,23 +185,23 @@ def physicalCodes(SIN):
         connection.close()
 
 
-@app.route("/createAccount", methods=['POST', 'GET'])  # not sure
+@app.route("/createAccount", methods=['POST', 'GET'])  # work in progress
 def createAccount():
     try:
-        _json = requests.json
+        connection = mysql.connect()
+        cursor = connection.cursor()
+        _json = request.json
         _username = _json['username']
         _password = _json['password']
         _professionName = _json['professionName']
-
-        query = "INSERT INTO ACCOUNT(username,password,professionName)"
-        data = (_username, _password, _professionName)
-        connection = mysql.connect()
-        cursor = connection.cursor()
-        cursor.execute(query, data)
-        cursor.commit()
-        result = jsonify("ProfessionalCreated")
-        result.status_code = 200
-        return result
+        if _username and _password and _professionName and request.method == 'POST':
+            query = "INSERT INTO ACCOUNT(username, password, professionName)"
+            data = (_username, _password, _professionName)
+            cursor.execute(query, data)
+            cursor.commit()
+            result = jsonify("Professional Created")
+            result.status_code = 200
+            return result
     except Exception as e:
         print(e)
     finally:
@@ -201,8 +214,53 @@ def getLog(logShareCode):
     try:
         connection = mysql.connect()
         cursor = connection.cursor()
-        query = "SELECT * FROM LOG_BOOK WHERE logShareCode = %s"
+        query = " SELECT day, month, year, event, behaviour, actionsTaken, YSIN FROM LOG_CODES as L1, LOG_BOOK as L2" \
+                " WHERE L2.logShareCode = %s and L1.logCode = L2.logShareCode"
         info = logShareCode
+        cursor.execute(query, info)
+        canView = cursor.fetchone()
+        result = jsonify(canView)
+        result.status_code = 200
+        return result
+    except Exception as e:
+        print(e)
+        print('Error: no log book found')
+    finally:
+        cursor.close()
+        connection.close()
+
+
+@app.route("/getMHE/<mentalShareCode>", methods=['GET'])  # get JSON object of a log book document using share code
+def getMHE(mentalShareCode):
+    try:
+        connection = mysql.connect()
+        cursor = connection.cursor()
+        query = "SELECT * FROM MENTAL_CODES as P1, MENTAL_HEALTH_EVALUATION as P2, THERAPY as P3, SYMPTOMS as P4" \
+                "WHERE P1.mentalCode = P2.mentalShareCode and P2.mentalShareCode = P3.mentalShareCode " \
+                "and P2.mentalShareCode = P4.mentalShareCode and P1.mentalCode = %s"
+        info = mentalShareCode
+        cursor.execute(query, info)
+        canView = cursor.fetchone()
+        result = jsonify(canView)
+        result.status_code = 200
+        return result
+    except Exception as e:
+        print(e)
+        print('Error: no log book found')
+    finally:
+        cursor.close()
+        connection.close()
+
+
+@app.route("/getPHE/<physicalShareCode>", methods=['GET'])  # get JSON object of a log book document using share code
+def getPHE(physicalShareCode):
+    try:
+        connection = mysql.connect()
+        cursor = connection.cursor()
+        query = " SELECT day, month, year, weight, height, temperature, heartRate, bloodPressure, respiratoryRate, pedSIN, name, dosage, dosesPerDay, illness" \
+                " FROM PHYSICAL_CODES as P1, PHYSICAL_HEALTH_EVALUATION as P2, PRESCRIPTION as P3" \
+                " WHERE P1.physicalCode = P2.physicalShareCode and P2.physicalShareCode = P3.physicalShareCode and P1.physicalCode = %s"
+        info = physicalShareCode
         cursor.execute(query, info)
         canView = cursor.fetchone()
         result = jsonify(canView)
